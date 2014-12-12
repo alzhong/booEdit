4193b32ba7e1935be5fd974e1eb30f65b62bd467
ics45c <aaronzhong.edu@gmail.com>
2014-12-12 14:23:01 -0800

    First build

---
+++ out/CMakeFiles/a.out.gtest.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ics45c/projects/booEdit/app/BooEdit.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/app/BooEdit.cpp.o"
+  "/home/ics45c/projects/booEdit/app/BooEditLog.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/app/BooEditLog.cpp.o"
+  "/home/ics45c/projects/booEdit/app/CommandProcessor.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/app/CommandProcessor.cpp.o"
+  "/home/ics45c/projects/booEdit/app/Editor.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/app/Editor.cpp.o"
+  "/home/ics45c/projects/booEdit/app/EditorView.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/app/EditorView.cpp.o"
+  "/home/ics45c/projects/booEdit/app/Keypress.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/app/Keypress.cpp.o"
+  "/home/ics45c/projects/booEdit/gtest/gtestmain.cpp" "/home/ics45c/projects/booEdit/out/CMakeFiles/a.out.gtest.dir/gtest/gtestmain.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../app"
+  "../gtest"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
